bd47de44f11da8e2ee9d376340fb58a51411cabc
alberto <alcalino_78@hotmail.com>
2023-03-14 18:48:22 +0100

    delete personal info

--- src/CV/CV.js
+++ src/CV/CV.js
@@ -1,68 +1,31 @@
 export const CV = {
     personalInfo: {
-      name: "Alberto Ortiz Treviño",
-      mail: "alcalino_78@hotmail.com",
-      charge: "Arquitecto CSS - Frontend Developer",
-      phone: "666666666",
-      image: '/src/assets/foto-alberto-ortiz-trevino.jpg',
-      linkedin: "https://www.linkedin.com/in/alberto-ortiz-trevino/",
+      name: "",
+      mail: "",
+      charge: "",
+      phone: "",
+      image: '',
+      linkedin: "",
     },
     education: [
       {
-        nameEstudies: "Curso de Angular 2",
-        place: "FICTIZIA",
-        start: 'SEP 2017',
-        end: 'ENE 2018',
-      },
-      {
-        nameEstudies: "curso de desarrollo web html5 y css3",
-        place: "Imagina Formación",
-        start: 'JUN 2013',
-        end: 'JUL 2013',
-      },
-      {
-        nameEstudies: "curso de usabilidad y accesibilidad",
-        place: "Universidad de Murcia",
-        start: 'FEB 2009',
-        end: 'ABR 2009',
-      },
-      {
-        nameEstudies: "máster de autoedición gráfica",
-        place: "Estudio de Diseño Gráfico PUNTAFINA",
-        start: 'MAY 2005',
-        end: 'JUL 2005',
-      },
-      {
-        nameEstudies: "BUP - COU",
-        place: "IES Cardenal Cisneros",
-        start: '1993',
-        end: '1997',
+        nameEstudies: "",
+        place: "",
+        start: '',
+        end: '',
       }
     ],
     experience: [
       {
-        nameJob: "Arquitecto CSS - Responsable Front End",
-        companyName: "Sanitas",
-        date: 'SEP 2016 - JUL 2022',
-        description: 'Responsable junto mi equipo de la arquitectura CSS y la parte del código HTML de todos los sites de la parte pública de Sanitas.es, mediante la utilización de un Framewrok CSS que he creado basándome en los principios de desarrollo de software.'
-      },
-      {
-        nameJob: "Maquetador Web",
-        companyName: "Sanitas",
-        date: 'NOV 2013 - SEP 2016',
-        description: 'Maquetación RWD de la web de Sanitas.es'
-      },
-      {
-        nameJob: "Maquetador Web",
-        companyName: "Open Sistemas",
-        date: 'JUL 2013 - SEP 2016',
-        description: 'Diseño, maquetación e implementación de portales web con Joomla y Wordpress.'
+        nameJob: "",
+        companyName: "",
+        date: '',
+        description: ''
       }
     ],
     languages: [
       {
         nameEstudies: "Inglés",
-    
         knowledge: "B2",
         start: 2015,
         end: new Date().getFullYear(),
